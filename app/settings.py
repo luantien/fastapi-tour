e4470d085e491878e4da377d67ec0616efa0beb1
Tien Nguyen <luantien411@gmail.com>
2024-08-19 16:17:36 +0700

    Update some suggestion comment

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,16 @@
+""" Application Settings Module
+"""
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection_string():
+    """Get the connection string for the database
+
+    Returns:
+        string: The connection string
+    """
     engine = os.environ.get("DB_ENGINE")
     dbhost = os.environ.get("DB_HOST")
     username = os.environ.get("DB_USERNAME")
@@ -18,3 +25,5 @@ ADMIN_DEFAULT_PASSWORD = os.environ.get("DEFAULT_PASSWORD")
 # JWT Setting
 JWT_SECRET = os.environ.get("JWT_SECRET")
 JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM")
+
+print(get_connection_string())
